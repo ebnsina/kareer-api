618039ce2160b8507f7dc520d008e0834d98bfd2
Ebn Sina <ebnsina@amharctech.com>
2025-06-09 21:38:40 +0600

    update all routes

--- main.ts
+++ main.ts
@@ -6,6 +6,14 @@ import cors from "npm:cors";
 import { Server } from "npm:socket.io";
 import { CLIENT_URL, DATABASE_URI, PORT } from "./lib/constants.ts";
 import { connectToDB } from "./lib/db.ts";
+import adminRoutes from "./routes/admin.route.ts";
+import applicationRoutes from "./routes/application.route.ts";
+import authRoutes from "./routes/auth.route.ts";
+import bookmarkRoutes from "./routes/bookmark.route.ts";
+import companyRoutes from "./routes/company.route.ts";
+import jobRoutes from "./routes/job.route.ts";
+import notificationRoutes from "./routes/notification.route.ts";
+import userRoutes from "./routes/user.route.ts";
 
 if (!PORT) {
   console.warn(
@@ -43,9 +51,14 @@ app.set("io", io);
 
 connectToDB();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Kareer API!");
-});
+app.use("/api/admin", adminRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/jobs", jobRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/applications", applicationRoutes);
+app.use("/api/companies", companyRoutes);
+app.use("/api/bookmarks", bookmarkRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
